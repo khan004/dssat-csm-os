bb2f5262d0f7412dde22cccaeaf92baaadd4403b
Muhammad Sohail Khan <sohail.wur@gmail.com>
2024-10-06 23:59:17 +0200

    PT_MATURITY: calculate maturity based on dry weight

--- Plant/SUBSTOR-Potato/PT_MATURITY.for
+++ Plant/SUBSTOR-Potato/PT_MATURITY.for
@@ -24,7 +24,7 @@ C-----------------------------------------------------------------------
       INTEGER DYNAMIC, YRDOY, MDATE, TB
       
       REAL TUBWT, PLTPOP
-      REAL FRYLD
+      REAL FRYLD, DRYLD
       REAL DTT, STT, TDIFF
       REAL W, WB, WPREV, WDIFF
       REAL CM, RM, WDIFFRATE
@@ -34,11 +34,11 @@ C-----------------------------------------------------------------------
       
       TYPE (ControlType) CONTROL
       
-      
       YRDOY  = CONTROL % YRDOY
       DYNAMIC = CONTROL % DYNAMIC
 
       FRYLD = 0.0
+      DRYLD = 0.0
       W = 0.0
       WDIFF = 0.0
       WDIFFRATE = 0.0
@@ -59,13 +59,20 @@ C-----------------------------------------------------------------------
       RM = 0.34 ! Relative growth rate in exponential phase g/m^2
 
       FRYLD = (TUBWT*10.*PLTPOP/1000.)/0.2   ! Fresh yield, from PT_OPGROW L.No. 273
+
+      ! Dry yield = 20% of fresh yield
+      DRYLD = FRYLD * 0.2
       
-!     FRYLD is Mg/Ha while while CM and RM are in g/m^2
-!     Multiply by 0.01 for g/m^2 to Mg/Ha (1Ha = 10000 m^2,
+!     Fresh/dry yield weight is in Mg/Ha while while CM and RM are based
+!     on weight taken in units g/m^2
+!     Multiply by 0.01 to convert g/m^2 to Mg/Ha
+!     (1Ha = 10000 m^2,
 !     1Mg=1000000g)
+
       WB = ((CM/RM)*log(2.0)) * 0.01         ! Tuber weight at TB
 
-      W = FRYLD   ! uncommented: based on fresh yield
+      ! W = FRYLD
+      W = DRYLD   ! based on dry yield
       TDIFF = STT ! REF to PT_GROSUB L.No 515; 628)
 
       ! Find first point (Tb) where W greater than or equal to Wb
